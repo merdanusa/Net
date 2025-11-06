01f019369d4aacc46f5538a435f3968c936b2846
Merdan <wisew.tube@gmail.com>
2025-11-06 17:31:41 +0500

    init commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My Black App',
+      title: 'VPN',
       theme: ThemeData.dark(),
       home: const AuthPage(),
       onGenerateRoute: (settings) {
