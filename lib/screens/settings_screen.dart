be54c8ccbabe186c1714fef014fbca501b00c099
Merdan <wisew.tube@gmail.com>
2025-11-20 18:14:37 +0500

    init commit

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,15 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:go_router/go_router.dart';
+import 'package:vpn/controllers/auth_controllers.dart';
 
-class SettingsScreen extends StatefulWidget {
+class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
 
   @override
-  State<SettingsScreen> createState() => _SettingsScreenState();
-}
-
-class _SettingsScreenState extends State<SettingsScreen> {
-  @override
   Widget build(BuildContext context) {
-    return Container();
+    final auth = Get.find<AuthController>();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Settings'),
+        backgroundColor: const Color(0xFF0A0A0A),
+      ),
+      backgroundColor: const Color(0xFF0A0A0A),
+      body: ListView(
+        padding: const EdgeInsets.all(16),
+        children: [
+          ListTile(
+            leading: const Icon(Icons.logout, color: Colors.red),
+            title: const Text('Logout', style: TextStyle(color: Colors.red)),
+            onTap: () {
+              auth.clearToken();
+              context.go('/login');
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
